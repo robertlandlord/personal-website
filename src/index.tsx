9d47b135dbe31176c513fbff979aead267276afc
Robert Landlord <robertlandlord1@gmail.com>
2024-01-19 00:13:36 -0800

    make a 3js box

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './views/home/App';
+import App from './views/home/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
